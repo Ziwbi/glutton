b369c272af3ef7e56e87e23adf32b308ab24cd6d
Ziwbi <cd2bfe@gmail.com>
2025-06-24 19:15:44 +0100

    fixed seed worldgen

--- modworldgenmain.lua
+++ modworldgenmain.lua
@@ -1,32 +1,36 @@
-print("### Glutton Mod World Gen Main ###")
+local modenv = env
+GLOBAL.setfenv(1, GLOBAL)
 
+local seed = 83445 -- change this if you want to try a different map
 
-local map_type = GetModConfigData( "map_gen" )
+local map_type = modenv.GetModConfigData("map_gen")
 if map_type == "fixed" then
-	print("Setting world gen random seed")
-	math.randomseed(83445)
+    print("Setting world gen random seed")
+    -- SetWorldGenSeed is not defined yet :/
+    math.randomseed(seed)
+	math.random()
+    SEED = seed
 end
 
-
-AddLevel( "LEVELTYPE_GLUTTON", {
-		id = "GLUTTON",
-		name = "Glutton preset",
-		desc = "Glutton preset description!",
-        location = "forest",
-        version = 2,
-        overrides={
-			flint = "often",
-			grass = "often",
-			sapling = "often",
-			carrot = "often",
-			mushroom = "often",
-			spiders = "often",
-			bees = "often",
-			birds = "often",
-			butterfly = "always",
-			ponds = "often",
-			pigs = "often",
-			rock = "often",
-			rabbits = "always",
-        },
-	})
+modenv.AddLevel("LEVELTYPE_GLUTTON", {
+    id = "GLUTTON",
+    name = "Glutton preset",
+    desc = "Glutton preset description!",
+    location = "forest",
+    version = 2,
+    overrides = {
+        flint = "often",
+        grass = "often",
+        sapling = "often",
+        carrot = "often",
+        mushroom = "often",
+        spiders = "often",
+        bees = "often",
+        birds = "often",
+        butterfly = "always",
+        ponds = "often",
+        pigs = "often",
+        rock = "often",
+        rabbits = "always",
+    },
+})
