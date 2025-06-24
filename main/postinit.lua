56f7eed6bcf5e2d9e343ace300a9c3605c14eaa1
Ziwbi <cd2bfe@gmail.com>
2025-06-24 19:32:21 +0100

    world_network

--- main/postinit.lua
+++ main/postinit.lua
@@ -1,5 +1,6 @@
 local prefab_postinits = {
     "player",
+    "world_network",
 }
 
 for k, v in pairs(prefab_postinits) do
--- modmain.lua
+++ modmain.lua
@@ -2,39 +2,7 @@
 local modenv = env
 GLOBAL.setfenv(1, GLOBAL)
 
-local world_net_inst = nil
-
-function GameSetup(inst)
-    ANNOUNCEMENT_LIFETIME = 3
-    ANNOUNCEMENT_FADE_TIME = 1
-    ANNOUNCEMENT_QUEUE_SIZE = 4
-
-    if TheNet:GetServerGameMode() == "glutton" then
-        print("### Glutton Mode ###")
-
-        inst.game_flow = modenv.GetModConfigData("game_flow")
-        inst.game_time = modenv.GetModConfigData("game_time")
-
-        inst:AddComponent("GluttonGameLogic")
-        world_net_inst = inst
-    else
-        print("Not enabling glutton because the server game mode isn't matching. To enable this for dedicated servers, set game_mode = glutton in the [network] section of your settings.ini file.")
-    end
-end
---Currently there is no support for a generic world_network postinit.
-modenv.AddPrefabPostInit("cave_network", GameSetup)
-modenv.AddPrefabPostInit("forest_network", GameSetup)
-
-
-modenv.AddReplicableComponent( "sprinter" )
-
-
-function StartGlutton()
-    assert(world_net_inst ~= nil)
-    print("### StartGlutton ###")
-    world_net_inst.components.GluttonGameLogic:StartGlutton()
-end
-
+modenv.AddReplicableComponent("sprinter")
 
 modenv.modimport("main/postinit.lua")
 modenv.modimport("main/RPC.lua")
